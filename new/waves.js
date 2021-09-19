3d8aff1fb9e3eef5266f23ac8592f667b5c1893b
Moishe Lettvin <moishel@gmail.com>
2021-09-19 07:11:01 -0600

    not quite there

--- new/waves.js
+++ new/waves.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var gl;
+var colors = [];
 var pixels = [];
 var iH;
 var iW;
@@ -8,12 +9,13 @@ var canvas;
 
 const maxAge = 100;
 
-const initialActors = 10000;
+const initialActors = 10;
 const maxPixels = 100000;
 var numPixels = 0;
 
 var actors = [];
 var deposits;
+var trail = [];
 
 function main() {
   // Get A WebGL context
@@ -34,6 +36,24 @@ function main() {
 
   // look up uniform locations
   var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+
+  for (var i = 0; i < maxPixels; i++) {
+    colors.push(i / maxPixels)
+    colors.push(i / maxPixels)
+    colors.push(i / maxPixels)
+    colors.push(1)  
+  }
+
+  colors = new Float32Array(colors);
+  
+  var colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, colors, gl.DYNAMIC_DRAW);
+  
+  var colorsLocation = gl.getAttribLocation(program, "colors");
+  gl.vertexAttribPointer(colorsLocation, 4, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(colorsLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
   // Create a buffer to put three 2d clip space points in
   var positionBuffer = gl.createBuffer();
@@ -60,15 +80,14 @@ function main() {
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
   for (var i = 0; i < maxPixels; i++) {
-    pixels.push(0); // x
-    pixels.push(0); // y
-    pixels.push(0); // a
+    pixels.push(0);
+    pixels.push(0);
   }
 
   pixels = new Float32Array(pixels);
 
   // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-  var size = 3;          // 3 components per iteration
+  var size = 2;          // 2 components per iteration
   var type = gl.FLOAT;   // the data is 32bit floats
   var normalize = false; // don't normalize the data
   var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
@@ -79,6 +98,7 @@ function main() {
   // set the resolution
   gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
+  initializeActors();
   initializeDeposits();
 
   animate();  
@@ -87,7 +107,7 @@ function main() {
 function initializeActors() {
   for (var i = 0; i < initialActors; i++) {
     actors[i] = {
-      'position': [randomInt(iW), randomInt(iH)],
+      'position': [randomInt(gl.canvas.width), randomInt(gl.canvas.height)],
       'direction': Math.random() * Math.PI * 2,
       'speed': 1,
       'age': randomInt(10)
@@ -111,21 +131,12 @@ function initializeDeposits() {
     var vRatio = canvas.height / img.height;
     var ratio  = Math.min ( hRatio, vRatio );
     
-    iH = Math.floor(img.height * ratio);
-    iW = Math.floor(img.width * ratio);
+    iH = img.height * ratio;
+    iW = img.width * ratio;
   
     context.drawImage(img, 0, 0, img.width, img.height, 0, 0, img.width * ratio, img.height * ratio);
     var data = context.getImageData(0, 0, img.width, img.height).data;
-
-    deposits = [];
-    for (var i = 0; i < iH * iW; i++) {
-      var v = 0; //(data[i * 3] + data[i * 3 + 1], data[i * 3 + 2]) / (255.0 * 3.0);
-      deposits.push(v)
-    }
-
-    deposits = new Float32Array(deposits);
-
-    initializeActors();
+    deposits = data;
   }
 }
 
@@ -135,81 +146,50 @@ function animate() {
   drawScene();
 }
 
-const lookDirection = Math.PI / 3;
-const lookMomentum = 1.1;
-
-function getMoveCoords(actor, distance, direction) {
-  var look_x = actor['position'][0] + distance * Math.cos(actor['direction'] + direction);
-  var look_y = actor['position'][1] + distance * Math.sin(actor['direction'] + direction);
-  return [look_x, look_y];
-}
-
-function getDepositValue(coords) {
-  var deposit_idx = Math.floor(coords[0]) + Math.floor(coords[1]) * iW;
-  return deposits[deposit_idx];
-}
-
-function getDirectionalValues(actor) {
-  var lcoords = getMoveCoords(actor, 1, -lookDirection);
-  var lv = getDepositValue(lcoords);
-
-  var rcoords = getMoveCoords(actor, 1, lookDirection);
-  var rv = getDepositValue(rcoords);
-
-  var mcoords = getMoveCoords(actor, 1, 0);
-  var mv = getDepositValue(mcoords);
-
-  return {'lv': lv, 'mv': mv, 'rv': rv};
-}
-
-function incrementDepositValue(coords, amount) {
-  var deposit_idx = coords[0] + coords[1] * iW;
-  deposits[deposit_idx] += 0.1;
-}
-
 function processActors() {
-  if (!deposits) {
-    return;
-  }
-
-  var new_actors = [];
   var idx = 0;
-
+  var new_actors = [];
   actors.forEach(actor => {
     if (idx < maxPixels) {
-      var new_actor = {
-        'position': [],
-        'speed': actor['speed'],
-        'direction': actor['direction']
-      }
-      var {lv, mv, rv} = getDirectionalValues(actor);
-      var dr;
-      if (lv > mv && lv > rv) {
-        dr = -1;
-      } else if (rv > mv && rv > lv) {
-        dr = 1
-      } else {
-        dr = 0;
-      }
-      new_actor['direction'] += dr * lookDirection * lookMomentum;;
-      pixels[idx * 3] = actor['position'][0];       // x
-      pixels[idx * 3 + 1] = actor['position'][1];   // y
+      pixels[idx * 2] = actor['position'][0];
+      pixels[idx * 2 + 1] = actor['position'][1];
 
-      pixels[idx * 3 + 2] = getDepositValue(actor['position']);
-      incrementDepositValue(actor['position'], Math.max(lv, mv, rv));
+      if ((Math.random() * maxPixels) > Math.max(idx, numPixels)) {
+        var new_actor = {
+          'position' : [], 
+          'direction': actor['direction'] + (Math.random() - 0.5) * 0.4, 
+          'speed': actor['speed'] * 1.8 + 1,
+          'age': 50 + randomInt(100)
+        };
 
-      new_actor['position'][0] = actor['position'][0] + new_actor['speed'] * Math.cos(new_actor['direction']);
-      new_actor['position'][1] = actor['position'][1] + new_actor['speed'] * Math.sin(new_actor['direction']);
+        for (var i = 0; i < 2; i++) {
+          new_actor['position'][i] = actor['position'][i];
+        }
 
-      if (new_actor['position'][0] > 0 &&
-          new_actor['position'][0] < iW &&
-          new_actor['position'][1] > 1 &&
-          new_actor['position'][1] < iH) {
+        //new_actor['age'] = 50; //getStartingSpeedFromPosition(actor['position']);
         new_actors.push(new_actor);
+        colors[idx * 4] = actor['age'];
+        colors[idx * 4 + 1] = actor['age'];
+        colors[idx * 4 + 2] = 255 - actor['age'];
+      }
+
+      // are there any pheromones along the direction we're looking?
+      var s = 4; actor['speed'];
+      var d = actor['direction'];
+      actor['position'][0] += s * Math.cos(d);
+      actor['position'][1] += s * Math.sin(d);
+
+      if (actor['position'][0] > 0 &&
+          actor['position'][1] > 0 &&
+          actor['position'][0] < gl.canvas.width &&
+          actor['position'][1] < gl.canvas.height &&
+          actor['age'] > 0) {
+        actor['age'] -= 1;
+        new_actors.push(actor);
       }
     }
+
     idx++;
-    actors = new_actors;
   })
 
   actors = new_actors;
